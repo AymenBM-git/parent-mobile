259a8fb1ddeedc7155cfc65e59bb956fc895af11
AymenBM-git <isg.mpssi.0910@gmail.com>
2026-01-15 21:19:54 +0100

    api

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,6 @@
 // Replace with your computer's local IP address (e.g., 192.168.1.XX)
 // You can find it by running 'ipconfig' in your terminal
-export const BASE_URL = 'http://192.168.1.8:3000';
+export const BASE_URL = 'https://leaders-boumhel.vercel.app'///'http://192.168.1.8:3000';
 
 export const apiFetch = async (endpoint: string, options: any = {}) => {
     const url = endpoint.startsWith('http') ? endpoint : `${BASE_URL}${endpoint}`;
